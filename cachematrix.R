83a0991535f0a69a685ad708c491daf98f6cfa57
kenkimmel <kenkimmel@users.noreply.github.com>
2014-12-20 12:56:58 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -9,10 +9,11 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m<-NULL
-        set<-function(y){
-        x<<-y
-        m<<-NULL
-}
+                set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        
         get<-function() x
         setmatrix<-function(solve) m<<- solve
         getmatrix<-function() m
