b07fa548f7b083c74d5f979acd9fdd5e8d6f1f30
kenkimmel <kenkimmel@users.noreply.github.com>
2014-12-20 12:23:31 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The combined functions allow a user to create a cache-able
+## matrix that then can be used by the second function to create
+## the inverse of the cache-able matrix and save it to a cache, 
+## allowing the function to save computing time by retrieving the 
+## cached inverse if it is available
 
 ## Write a short comment describing this function
 
@@ -13,4 +16,4 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
-##test
+
